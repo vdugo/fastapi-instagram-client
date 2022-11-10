6166b40d52a215841f406ef4437be83f5b80cd2e
Vincent <v3rdugo@yahoo.com>
2022-11-10 14:44:06 -0800

    fetching the posts from the api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const BASE_URL = 'http://localhost:8000'
+
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(BASE_URL + '/post/all')
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response
+    })
+    .then((data) => {
+      setPosts(data)
+    })
+    .catch((error) => {
+      console.log(error)
+      alert(error)
+    })
+  }, [])
+
   return (
     <div className="App">
       Hello World
