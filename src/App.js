322b42fabf1c841d4c2b0419d4d4d76b0df9355c
Vincent <v3rdugo@yahoo.com>
2022-11-10 21:07:23 -0800

    make post component look better with header and delete button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,17 @@ function App() {
       }
       throw response
     })
+    // sort the posts by timestamp
+    .then((data) => {
+      const result = data.sort((a,b) => {
+        const time_a = a.timestamp.split(/[-T:]/)
+        const time_b = b.timestamp.split(/[-T:]/)
+        const date_a = new Date(Date.UTC(time_a[0], time_a[1]-1, time_a[2], time_a[3], time_a[4], time_a[5]))
+        const date_b = new Date(Date.UTC(time_b[0], time_b[1]-1, time_b[2], time_b[3], time_b[4], time_b[5]))
+        return date_b - date_a
+      })
+      return result
+    })
     .then((data) => {
       setPosts(data)
     })
@@ -28,11 +39,11 @@ function App() {
   return (
     <div className="app_posts">
       {
-        posts.map((post) => {
+        posts.map((post) => (
           <Post
           post = {post}
           />
-        })
+        ))
       }
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -2,9 +2,58 @@ import { useState, useEffect } from "react";
 
 import './Post.css'
 
+import { Avatar, Button } from "@material-ui/core";
+
+const BASE_URL = 'http://localhost:8000/'
+
 const Post = ({ post }) => {
+
+  const [imageUrl, setImageUrl] = useState('')
+  const [comments, setComments] = useState([])
+  
+  useEffect(() => {
+    if (post.image_url_type === 'absolute') {
+        setImageUrl(post.image_url)
+    }
+    else {
+        setImageUrl(BASE_URL + post.image_url)
+    }
+  }, [])
+
+  useEffect(() => {
+    setComments(post.comments)
+  }, [])
+
+
   return (
-    <div>Post</div>
+    <div className="post">
+        <div className="post_header">
+            <Avatar
+            alt="avatar"
+            src=""
+            />
+            <div className="post_headerInfo">
+                <h3>{post.user.username}</h3>
+                <Button className="post_delete">Delete</Button>
+            </div>
+        </div>
+        <img
+        className="post_image"
+        src={imageUrl}
+        />
+
+        <h4 className="post_text">{post.caption}</h4>
+
+        <div className="post_comments">
+            {
+                comments.map((comment) => (
+                    <p>
+                        <strong>{comment.username}:</strong> {comment.text}
+                    </p>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
